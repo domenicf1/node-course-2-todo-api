8f5eb2bffe10a9782d704bfa710d9d3675d2d8b9
Domenic Filippone <domenicf@Domenics-MBP.home>
2018-07-19 21:58:03 -0400

    deplyt to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://admin:admin1@ds141611.mlab.com:41611/still-peak-61965-todo" || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
