df86c8babe6b60fd288949e321953dc765e8cdaf
Domenic Filippone <domenicf@Domenics-MBP.home>
2018-12-20 16:50:00 -0500

    Added DELETE token and tests

--- server/server.js
+++ server/server.js
@@ -113,7 +113,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-//POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -123,6 +122,14 @@ app.post('/users/login', (req, res) => {
         });
     }).catch((err) => {
         res.status(400).send(err);
+    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }, () => {
+        res.status(400).send()
     });
 });
 
